e89562fbbb569e9e23988fcc86fc2e896ffa03a8
premchandm <40263973+premchandm@users.noreply.github.com>
2022-01-30 23:56:55 +0800

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def get_value(object,k):
+    for key, value in object.items():
+        if type(value) is dict:
+            get_value(value,k)
+            if key == k:
+                print (value)
+        else:
+            if key == k:
+                print (value)
+
+
+r = requests.get("http://169.254.169.254/latest/meta-data/")
+r = r.json()
+print(json.dumps(r,indent=2))
+key = input("Enter Key: ")
+get_value(r,key)
+
+http://169.254.169.254/latest/meta-data/
+
+#For testing the request can be replaced with ("http://headers.jsontest.com")
